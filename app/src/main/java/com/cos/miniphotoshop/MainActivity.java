61adbe52f253ad853b29e6cd4e5407011b51ebd2
ruinak <teezeei@naver.com>
2021-09-04 19:49:26 +0900

    확대 및 축소 아이콘 코딩

--- app/src/main/java/com/cos/miniphotoshop/MainActivity.java
+++ app/src/main/java/com/cos/miniphotoshop/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     // 위젯 변수 6개와 클래스 변수 1개를 전역변수로 선언
     private ImageButton ibZoomIn, ibZoomOut, ibRotate, ibBright, ibDark, ibGray;
     private MyGraphicView graphicView;
+    // 축척으로 사용될 전역변수 2개를 선언
+    private static float scaleX = 1, scaleY = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,29 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutCompat pictureLayout = findViewById(R.id.pictureLayout);
         graphicView = new MyGraphicView(this);
         pictureLayout.addView(graphicView);
+
+        init();
+        initLr();
+    }
+
+    public void init(){
+        ibZoomIn = findViewById(R.id.ibZoomIn);
+        ibZoomOut = findViewById(R.id.ibZoomOut);
+    }
+
+    public void initLr(){
+        ibZoomIn.setOnClickListener(v -> {
+            scaleX = scaleX + 0.2f;
+            scaleY = scaleY + 0.2f;
+            // 확대를 위해서 onDraw( ) 메서드를 다시 호출해야하는데
+            // 뷰의 invalidate( ) 메서드는 onDraw( )를 자동으로 호출함
+            graphicView.invalidate();
+        });
+        ibZoomOut.setOnClickListener(v -> {
+            scaleX = scaleX - 0.2f;
+            scaleY = scaleY - 0.2f;
+            graphicView.invalidate();
+        });
     }
 
     // MyGraphicView 클래스를 정의
@@ -40,6 +65,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
+
+            // 화면(뷰)의 중앙을 구하고, 전역변수에 설정된 값으로 캔버스의 축척을 설정
+            int cenX = this.getWidth() / 2;
+            int cenY = this.getHeight() / 2;
+            canvas.scale(scaleX, scaleY, cenX, cenY);
 
             Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.umbrella);
 
